97c06ac028764c6063c760a2c4784b7f49192016
Sak, Andrey2 <a2.sak@itransition.com>
2017-06-26 14:27:39 +0300

    init commit

---
+++ SSearchBar/SSearchBar/SSearchBar.h
@@ -0,0 +1,19 @@
+//
+//  SSearchBar.h
+//  SSearchBar
+//
+//  Created by Andrey Sak on 6/26/17.
+//  Copyright © 2017 Andrey Sak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSearchBar.
+FOUNDATION_EXPORT double SSearchBarVersionNumber;
+
+//! Project version string for SSearchBar.
+FOUNDATION_EXPORT const unsigned char SSearchBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSearchBar/PublicHeader.h>
+
+
