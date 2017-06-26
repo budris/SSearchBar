d33727fecfba8bc2fd4ed33d7abf91911caa18da
Sak, Andrey2 <a2.sak@itransition.com>
2017-06-26 14:35:02 +0300

    add sources

---
+++ SSearchBar/SSearchBar/SSearchBar.swift
@@ -0,0 +1,83 @@
+//
+//  SSearchBar.swift
+//  SSearchBar
+//
+//  Created by Andrey Sak on 6/26/17.
+//  Copyright © 2017 Andrey Sak. All rights reserved.
+//
+
+import UIKit
+
+protocol SSearchBarDelegate: UISearchBarDelegate {
+    func searchDidStart(_ searchBar: UISearchBar, didStartWith text: String?)
+}
+
+class SSearchBar: UISearchBar {
+    private static let defaultDelay: TimeInterval = 0.5
+    
+    fileprivate var timer: Timer?
+    
+    weak public var sSearchBarDelegate: SSearchBarDelegate?
+    
+    public var delayForStartingSearch: TimeInterval = SSearchBar.defaultDelay
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        delegate = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        delegate = self
+    }
+    
+    override func responds(to aSelector: Selector!) -> Bool {
+        if let sSearchBarDelegate = sSearchBarDelegate,
+            sSearchBarDelegate.responds(to: aSelector) {
+            return true
+        }
+        
+        return super.responds(to: aSelector)
+    }
+    
+    override func forwardingTarget(for aSelector: Selector!) -> Any? {
+        if let sSearchBarDelegate = sSearchBarDelegate,
+            sSearchBarDelegate.responds(to: aSelector) {
+            return sSearchBarDelegate
+        }
+        
+        return super.forwardingTarget(for: aSelector)
+    }
+    
+    func fire(_ timer: Timer) {
+        guard let searchBar = timer.userInfo as? UISearchBar else {
+            return
+        }
+        
+        let searchText = searchBar.text
+        sSearchBarDelegate?.searchDidStart(searchBar, didStartWith: searchText)
+    }
+    
+    public func stopTracking() {
+        timer?.invalidate()
+    }
+    
+}
+
+extension SSearchBar: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        sSearchBarDelegate?.searchBar?(searchBar, textDidChange: searchText)
+        
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(timeInterval: delayForStartingSearch,
+                                     target: self,
+                                     selector: #selector(SSearchBar.fire(_:)),
+                                     userInfo: searchBar,
+                                     repeats: false)
+    }
+    
+}
